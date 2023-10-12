a7c688ee908a071a93cd57f2f02fe9fb635a5406
MOHAMED3418O5 <132299682+MOHAMED3418O5@users.noreply.github.com>
2023-10-12 21:36:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let projects = document.getElementById("projects");
+projects.onmouseover = function () {
+  let header = document.getElementById("header");
+  header.style.backgroundColor = "white";
+};
+projects.onmouseout = function () {
+  let header = document.getElementById("header");
+  header.style.backgroundColor = "#ffffff00";
+};
+ 
+
+
